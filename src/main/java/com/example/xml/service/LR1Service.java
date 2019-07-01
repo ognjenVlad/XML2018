95e9e748046b1590f714e9f312176124889ff0a7
Nikola Stajic <n.stajic42@gmail.com>
2019-07-01 19:48:42 +0200

    Changed minor details of LR1 and added save and find by id functions for LR1.

--- src/main/java/com/example/xml/service/LR1Service.java
+++ src/main/java/com/example/xml/service/LR1Service.java
@@ -1,8 +1,10 @@
 package com.example.xml.service;
 
-import com.example.xml.DTO.LR1DTO;
+import com.example.xml.dtos.LR1DTO;
 
 public interface LR1Service {
 
-    void save(LR1DTO dto);
+    void save(LR1DTO lr1, String id);
+
+    LR1DTO findById(String id);
 }
--- src/main/java/com/example/xml/service/LR1ServiceImpl.java
+++ src/main/java/com/example/xml/service/LR1ServiceImpl.java
@@ -1,12 +1,33 @@
 package com.example.xml.service;
 
-import com.example.xml.DTO.LR1DTO;
+import com.example.xml.dtos.LR1DTO;
+import com.example.xml.repository.DocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LR1ServiceImpl implements LR1Service {
 
-    public void save(LR1DTO dto){
+    @Autowired
+    private DocumentRepository documentRepository;
 
+    public void save(LR1DTO lr1, String id){
+        try {
+            documentRepository.save(lr1, id);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
+
+    public LR1DTO findById(String id) {
+        try {
+            return documentRepository.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
 }
